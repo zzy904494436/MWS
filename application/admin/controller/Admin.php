f0af1105dc6f33894d0707e39b0dc92cdba83fae
zzy904494436 <zzy904494436@outlook.com>
2019-01-28 19:15:24 +0800

    增加一个管理员

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Db;
 
 class Admin extends Controller
 {
@@ -14,6 +15,21 @@ class Admin extends Controller
 
     public function add()
     {
+        if (request()->isPost()){
+            $data = [
+                'username' => input('username'),
+                'password' => md5(input('password'))
+            ];
+
+//            if (Db::name('admin')-> insert($data)){
+            if (\db('admin')-> insert($data)){
+                return $this->success("添加管理员成功",'lst');
+            }else{
+                return $this->error("添加管理员失败");
+            }
+
+            return;
+        }
         return $this->fetch();
     }
 
